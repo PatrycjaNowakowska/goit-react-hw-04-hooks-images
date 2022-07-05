a879caf1f4e226326f2ecd0535e1096e2f9e92ce
PatrycjaNowakowska <poczta.pnowakowska@gmail.com>
2022-07-05 23:57:19 +0200

    without initial view; fetch to fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,5 @@
-import React, { Component } from 'react';
-
+import React, { Component, useState, useEffect } from 'react';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
 import Loader from './Loader/Loader';
 import Button from './Button/Button';
 import ImageGallery from './ImageGallery/ImageGallery';
@@ -9,84 +7,72 @@ import Searchbar from './Searchbar/Searchbar';
 import getImages from 'utils/apiCalls';
 import Modal from './Modal/Modal';
 
-export class App extends Component {
-  state = {
-    searchInputValue: '',
-    tags: '',
-    images: [],
-    page: 1,
-    totalPages: 0,
-    webformatURL: '',
-    largeImageURL: '',
-    isLoading: false,
-    showModal: false,
-    showButton: true,
+
+function App() {
+  const [searchInputValue, setSearchInputValue] = useState('');
+  const [tags, setTags] = useState('');
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const [webformatURL, setWebformatURL] = useState('');
+  const [largeImageURL, setLargeImageURL] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [showButton, setShowButton] = useState(true);
+  const [error, setError] = useState(null);
+
+  const toogleLoading = () => {
+    setIsLoading(!isLoading);
   };
 
-  toogleLoading = () => {
-    this.setState(({ isLoading }) => ({
-      isLoading: !isLoading,
-    }));
+  const toogleModal = () => {
+    setShowModal(!showModal);
   };
 
-  toogleModal = () => {
-    this.setState(({ showModal }) => ({
-      showModal: !showModal,
-    }));
+  const inputValue = e => {
+    setSearchInputValue(e.target.value);
   };
 
-  inputValue = e => {
-    this.setState({ searchInputValue: e.target.value });
-  };
-
-  submitValue = e => {
+  const submitValue = e => {
     e.preventDefault();
-    this.fetchImages();
+    fetchImages();
   };
 
-  loadMore = e => {
+  const loadMore = e => {
     e.preventDefault();
-    this.fetchImages();
+    fetchImages();
   };
 
-  openModal = e => {
-    this.setState(() => ({
-      largeImageURL: e.target.dataset.source,
-      tags: e.target.alt,
-    }));
-    this.toogleModal();
+  const openModal = e => {
+    setLargeImageURL(e.target.dataset.source);
+    setTags(e.target.alt);
+    toogleModal();
   };
 
-  fetchMainView = () => {
-    fetch(
-      `https://pixabay.com/api/?key=11008307-2b498b2b7ae6a379a84554860&q=&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=9`
-    )
-      .then(data => data.json())
-      .then(searchInputValue => {
-        this.setState(({ images, showButton }) => ({
-          images: searchInputValue.hits,
-          showButton: true,
-        }));
-      })
-      .catch(error => console.log(error));
-  };
+  // const fetchMainView = () => {
+  //   fetch(
+  //     `https://pixabay.com/api/?key=11008307-2b498b2b7ae6a379a84554860&q=&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=9`
+  //   )
+  //     .then(data => data.json())
+  //     .then(searchInputValue => {
+  //       //setImages(searchInputValue.hits);
+  //       setShowButton(true);
+  //     })
+  //     .catch(error => console.log(error));
+  // };
 
-  fetchImages = async () => {
-    if (this.state.searchInputValue.trim() === '') {
+  const fetchImages = async () => {
+    if (searchInputValue.trim() === '') {
       return Notify.info('Put a keyword if you want search something');
     }
-    this.toogleLoading();
+    toogleLoading();
 
     try {
-      const request = await getImages(
-        this.state.searchInputValue,
-        this.state.page
-      );
-      this.setState(({ images, page, showButton }) => ({
-        images: [...images, ...request],
-        page: page + 1,
-        showButton: false,
-      }));
+      const request = await getImages(searchInputValue, page);
+      setImages([...images, ...request]);
+      //jak dobrać się do poprzednich images setImages(prevState => [...prevState, ...request.data.hits]);
+      setPage(page + 1);
+      // setShowButton(true);
 
       if (request.length === 0) {
         Notify.failure(
@@ -96,48 +82,200 @@ export class App extends Component {
     } catch (error) {
       Notify.failure('OOpsi. Something is wrong. Please, try again.');
     } finally {
-      this.toogleLoading();
+      toogleLoading();
     }
   };
 
-  componentDidMount() {
-    this.fetchMainView();
-  }
+  // useEffect(() => {
+  //   fetchMainView();
 
-  componentDidUpdate(_prevProps, prevState) {
-    if (prevState.searchInputValue !== this.state.searchInputValue) {
-      this.setState({ images: [], page: 1, error: null });
-    }
-  }
+  // });
 
-  render() {
-    return (
-      <div>
-        <Searchbar
-          onSubmit={this.submitValue}
-          inputValue={this.inputValue}
-          value={this.state.searchInputValue}
+  //   // const componentDidUpdate(_prevProps, prevState) {
+  //   //   if (prevState.searchInputValue !== this.state.searchInputValue) {
+  //   //     this.setState({ images: [], page: 1, error: null });
+  //   //   }
+  //   // }
+
+  return (
+    <div>
+      <Searchbar
+        onSubmit={submitValue}
+        inputValue={inputValue}
+        value={searchInputValue}
+      />
+      <ImageGallery images={images} openModal={openModal} />
+      {isLoading && <Loader />}
+
+      {showButton && isLoading && images.length >= 9 && (
+        <Button label={'Load more'} fetchMoreImages={loadMore} />
+      )}
+
+      {showModal && (
+        <Modal
+          largeImageURL={largeImageURL}
+          tags={tags}
+          toogleModal={toogleModal}
         />
-        <ImageGallery images={this.state.images} openModal={this.openModal} />
-        {this.state.isLoading && <Loader />}
-
-        {!this.state.showButton &&
-          !this.state.isLoading &&
-          this.state.images.length >= 9 &&
-          !this.state.error && (
-            <Button label={'Load more'} fetchMoreImages={this.loadMore} />
-          )}
-
-        {this.state.showModal && (
-          <Modal
-            largeImageURL={this.state.largeImageURL}
-            tags={this.state.tags}
-            toogleModal={this.toogleModal}
-          />
-        )}
-      </div>
-    );
-  }
+      )}
+    </div>
+  );
 }
-
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export class App extends Component {
+//   state = {
+//     searchInputValue: '',
+//     tags: '',
+//     images: [],
+//     page: 1,
+//     totalPages: 0,
+//     webformatURL: '',
+//     largeImageURL: '',
+//     isLoading: false,
+//     showModal: false,
+//     showButton: true,
+//   };
+
+  // toogleLoading = () => {
+  //   this.setState(({ isLoading }) => ({
+  //     isLoading: !isLoading,
+  //   }));
+  // };
+
+  // toogleModal = () => {
+  //   this.setState(({ showModal }) => ({
+  //     showModal: !showModal,
+  //   }));
+  // };
+
+  // inputValue = e => {
+  //   this.setState({ searchInputValue: e.target.value });
+  // };
+
+  // submitValue = e => {
+  //   e.preventDefault();
+  //   this.fetchImages();
+  // };
+
+  // loadMore = e => {
+  //   e.preventDefault();
+  //   this.fetchImages();
+  // };
+
+  // openModal = e => {
+  //   this.setState(() => ({
+  //     largeImageURL: e.target.dataset.source,
+  //     tags: e.target.alt,
+  //   }));
+  //   this.toogleModal();
+  // };
+
+  // fetchMainView = () => {
+  //   fetch(
+  //     `https://pixabay.com/api/?key=11008307-2b498b2b7ae6a379a84554860&q=&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=9`
+  //   )
+  //     .then(data => data.json())
+  //     .then(searchInputValue => {
+  //       this.setState(({ images, showButton }) => ({
+  //         images: searchInputValue.hits,
+  //         showButton: true,
+  //       }));
+  //     })
+  //     .catch(error => console.log(error));
+  // };
+
+  // fetchImages = async () => {
+  //   if (this.state.searchInputValue.trim() === '') {
+  //     return Notify.info('Put a keyword if you want search something');
+  //   }
+  //   this.toogleLoading();
+
+  //   try {
+  //     const request = await getImages(
+  //       this.state.searchInputValue,
+  //       this.state.page
+  //     );
+  //     this.setState(({ images, page, showButton }) => ({
+  //       images: [...images, ...request],
+  //       page: page + 1,
+  //       showButton: false,
+  //     }));
+
+  //     if (request.length === 0) {
+  //       Notify.failure(
+  //         'Sorry, there are no images matching your search. Please try something else.'
+  //       );
+  //     }
+  //   } catch (error) {
+  //     Notify.failure('OOpsi. Something is wrong. Please, try again.');
+  //   } finally {
+  //     this.toogleLoading();
+  //   }
+  // };
+
+//   componentDidMount() {
+//     this.fetchMainView();
+//   }
+
+//   componentDidUpdate(_prevProps, prevState) {
+//     if (prevState.searchInputValue !== this.state.searchInputValue) {
+//       this.setState({ images: [], page: 1, error: null });
+//     }
+//   }
+
+//   render() {
+    // return (
+    //   <div>
+    //     <Searchbar
+    //       onSubmit={this.submitValue}
+    //       inputValue={this.inputValue}
+    //       value={this.state.searchInputValue}
+    //     />
+    //     <ImageGallery images={this.state.images} openModal={this.openModal} />
+    //     {this.state.isLoading && <Loader />}
+
+    //     {!this.state.showButton &&
+    //       !this.state.isLoading &&
+    //       this.state.images.length >= 9 &&
+    //       !this.state.error && (
+    //         <Button label={'Load more'} fetchMoreImages={this.loadMore} />
+    //       )}
+
+    //     {this.state.showModal && (
+    //       <Modal
+    //         largeImageURL={this.state.largeImageURL}
+    //         tags={this.state.tags}
+    //         toogleModal={this.toogleModal}
+    //       />
+    //     )}
+    //   </div>
+    // );
+//   }
+// }
+
+// export default App;
